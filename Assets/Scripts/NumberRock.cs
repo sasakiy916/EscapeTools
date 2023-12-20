6bcca84cd73f860eadcaa7112933707ca907cef8
sasakiy916 <sasaki3y98521j@gmail.com>
2023-12-20 22:41:33 +0900

    追加 ナンバーロックの正解判定

--- Assets/Scripts/NumberRock.cs
+++ Assets/Scripts/NumberRock.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class NumberRock : MonoBehaviour
 {
     const int MAXNUM = 9;
+    [SerializeField, Range(0, MAXNUM)] int[] correctNumbers;
     [SerializeField, Range(0, MAXNUM)] int[] numbers;
     [SerializeField] TextMeshPro[] numberTexts;
+
+    // 数字の表示を反映させる
     public void SetNumbers()
     {
         for (int i = 0; i < numbers.Length; i++)
@@ -16,10 +19,23 @@ public class NumberRock : MonoBehaviour
         }
     }
 
+    // 数字を増やして、正解判定する
     public void IncreaseNum(int index)
     {
         numbers[index] = ++numbers[index] % (MAXNUM + 1);
         SetNumbers();
+        bool correct = CheckCorrectNumber();
+        Debug.Log(correct);
+    }
+
+    // 正解判定
+    public bool CheckCorrectNumber()
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] != correctNumbers[i]) return false;
+        }
+        return true;
     }
 
     void Start()

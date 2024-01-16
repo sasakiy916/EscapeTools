7ec80ea33746bd2ae0bc9c81f8491f03f09bc254
sasaki0719 <y.sasaki.roppongi@gmail.com>
2024-01-16 13:38:41 +0900

    追加：GreenRoomの出口側扉にナンバーチェック

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,22 +6,34 @@ using UnityEngine.EventSystems;
 
 public class Door : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] bool isLock;
     [SerializeField] bool isOpen;
+    [SerializeField] GameObject door;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("click");
-        if (isOpen) Open();
+        if (!isOpen) Open();
     }
 
     public void Open()
     {
-        if (!isOpen) transform.Rotate(new Vector3(0, 120, 0));
+        if (!isLock)
+        {
+            door.transform.Rotate(new Vector3(0, 120, 0));
+            isOpen = true;
+        }
+    }
+
+    public void UnLock()
+    {
+        isLock = false;
     }
 
     void Start()
     {
-
+        isOpen = false;
+        isLock = true;
     }
 
     void Update()
--- Assets/Scripts/NumberRock.cs
+++ Assets/Scripts/NumberRock.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NumberRock : MonoBehaviour
@@ -9,6 +10,8 @@ public class NumberRock : MonoBehaviour
     [SerializeField, Range(0, MAXNUM)] int[] correctNumbers;
     [SerializeField, Range(0, MAXNUM)] int[] numbers;
     [SerializeField] TextMeshPro[] numberTexts;
+    [SerializeField] Door door;
+    [SerializeField] Animator buttonAnimator;
 
     // 数字の表示を反映させる
     public void SetNumbers()
@@ -24,19 +27,22 @@ public class NumberRock : MonoBehaviour
     {
         numbers[index] = ++numbers[index] % (MAXNUM + 1);
         SetNumbers();
-        bool correct = CheckCorrectNumber();
-        Debug.Log(correct);
+        // bool correct = CheckCorrectNumber();
+        // Debug.Log(correct);
     }
 
     // 正解判定
-    public bool CheckCorrectNumber()
+    public void CheckCorrectNumber()
     {
+        buttonAnimator.SetTrigger("Push");
         for (int i = 0; i < numbers.Length; i++)
         {
-            if (numbers[i] != correctNumbers[i]) return false;
+            if (numbers[i] != correctNumbers[i]) return;
         }
-        return true;
+        door.UnLock();
+        // return true;
     }
+
 
     // Numberオブジェクトのアタッチ、それに合わせてフィールドの配列要素数変更
     public void AttachNumberTextObject()
@@ -46,13 +52,9 @@ public class NumberRock : MonoBehaviour
         correctNumbers = new int[numberTexts.Length];
     }
 
-    public class Quiz
-    {
-
-    }
     void Start()
     {
-
+        buttonAnimator = buttonAnimator.GetComponent<Animator>();
     }
 
     void Update()
